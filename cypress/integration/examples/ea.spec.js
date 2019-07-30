f27827dccb6a51685ccb21e2d7934c2bf1a6de76
Raul Velasco <raul93611@gmail.com>
2019-07-30 17:20:35 -0400

    I'm in video 9

--- cypress/integration/examples/ea.spec.js
+++ cypress/integration/examples/ea.spec.js
@@ -2,10 +2,20 @@
 context('Testing EA App', () => {
   it('Login application', () => {
     cy.visit('http://eaapp.somee.com/');
-    cy.contains('Login').click();
+    cy.get('#loginLink').invoke('text').as('text_login');
+
+    cy.get('#loginLink').click();
+
+    cy.get('@text_login').then(($text_login) => {
+      expect($text_login).is.eql('Login');
+    });
+
     cy.url().should('include', 'Account/Login');
     cy.get('#UserName').type('Admin');
     cy.get('#Password').type('password');
     cy.get('.btn').click();
+
+    cy.contains('Employee List').click();
+    cy.get('.table').find('tr').contains('Anuja').parent().contains('Benefits').click();
   });
 });
